181319017e786e75d90614278739db8bd663a152
user <andrey.gromov.36912@gmail.com>
2024-04-17 22:57:32 +0300

    Урок 12

--- Lesson_mood_12.py
+++ Lesson_mood_12.py
@@ -55,51 +55,115 @@
 # my_list.append(3)
 # my_list.append(4)
 # print(my_list)
+#
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.prev = None
+#         self.next = None
+#
+#     def __str__(self):
+#         return f"{self.data} -> {self.next}"
+#
+# class DoublyLinkedList:
+#     def __init__(self):
+#        self.head = None
+#
+#     def print(self):
+#         node = self.head
+#         while node is not None:
+#            print(node.data, end="->")
+#            node = node.next
+#
+#     def __str__(self):
+#       return str(self.head)
+#
+#     def append(self, data):
+#         new_node = Node(data)
+#
+#         if self.head is None:
+#             self.head = new_node
+#             return
+#
+#         tail = self.head
+#         while tail.next is not None:
+#
+#             tail = tail.next
+#
+#         tail.next = new_node
+#         new_node.prev = tail
+#
+#
+# my_list = DoublyLinkedList()
+# my_list.append(1)
+# my_list.append(2)
+# my_list.append(3)
+# print(my_list)
+# my_list.print()
 
 class Node:
     def __init__(self, data):
         self.data = data
-        self.prev = None
         self.next = None
+        self.prev = None
 
-    def __str__(self):
-        return f"{self.data} -> {self.next}"
-
-class DoublyLinkedList:
+class RegistrationQueue:# щдно звязний список
     def __init__(self):
-       self.head = None
-
-    def print(self):
-        node = self.head
-        while node is not None:
-           print(node.data, end="->")
-           node = node.next
-
-    def __str__(self):
-      return str(self.head)
+        self.head = None
+        self.tail = None
 
     def append(self, data):
         new_node = Node(data)
-
         if self.head is None:
             self.head = new_node
+            self.tail = new_node
+            return
+        self.tail.next = new_node
+        self.tail = new_node
+
+    def move_forward(self, data):
+        #Знайти node з потрібним data
+        node = self.head
+        while node is not None and node.data != data:
+            node = node.next
+
+
+        if node is None:
+            print("Немає пасажира у черзі")
             return
 
-        tail = self.head
-        while tail.next is not None:
+        # переміщення в перед
+        red = node.prev
+        green =node
+        blue =node.next
+        black = node.next.next
 
-            tail = tail.next
+        #змінюємо посилання в перд
+        red.next, green.next, blue.next, black.next = blue, black, green
 
-        tail.next = new_node
-        new_node.prev = tail
+        # змінюємо посилання  на зад
+         green.next, blue.prev, black.prev = blue, red, green
 
 
-my_list = DoublyLinkedList()
+    def print(self):
+        node = self.head
+
+        while node is not None:
+             print(node.data, and="->")
+             node = node.next
+
+
+my_list = BoardingQueue()
 my_list.append(1)
 my_list.append(2)
 my_list.append(3)
-print(my_list)
+my_list.append(4)
 my_list.print()
+my_list.move_forvard(3)
+my_list.print()
+class BoaringQueue:
+    pass
+
 
 
 
