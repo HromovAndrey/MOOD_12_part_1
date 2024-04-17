199c109a39cf87b428a5be0738631b456b68e459
user <andrey.gromov.36912@gmail.com>
2024-04-17 21:57:42 +0300

    Урок 12

--- Lesson_mood_12.py
+++ Lesson_mood_12.py
@@ -19,29 +19,87 @@
 
 
 
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+#
+#     def __str__(self):
+#         return f"{self.data} -> {self.next}"
+#
+# class LinkedList:
+#     def __init__(self):
+#        self.head = None
+#
+#     def __str__(self):
+#         return str(self.head)
+#
+#     def append(self,data):
+#         new_node = Node(data)
+#         if self.head is None:
+#             self.head = new_node
+#             return
+#
+#         tail = self.head
+#         while tail.next is not None:
+#          tail.next = new_node
+#
+#
+#         tail.next = new_node
+#         print(tail)
+#
+#
+# my_list = LinkedList()
+# my_list.append(1)
+# my_list.append(2)
+# my_list.append(3)
+# my_list.append(4)
+# print(my_list)
+
 class Node:
     def __init__(self, data):
         self.data = data
+        self.prev = None
         self.next = None
 
     def __str__(self):
         return f"{self.data} -> {self.next}"
 
-class LinkedList:
+class DoublyLinkedList:
     def __init__(self):
        self.head = None
 
-    def __str__(self):
-        return str(self.head)
+    def print(self):
+        node = self.head
+        while node is not None:
+           print(node.data, end="->")
+           node = node.next
 
-    def append(self. data):
-        new_node = None(data)
+    def __str__(self):
+      return str(self.head)
+
+    def append(self, data):
+        new_node = Node(data)
 
         if self.head is None:
             self.head = new_node
             return
 
+        tail = self.head
+        while tail.next is not None:
 
+            tail = tail.next
+
+        tail.next = new_node
+        new_node.prev = tail
+
+
+my_list = DoublyLinkedList()
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+print(my_list)
+my_list.print()
 
 
 
